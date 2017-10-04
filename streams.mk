5c69798e810f8da433e2a78337534dcc1a239cba
Anton Zaytsev <partyzan@list.ru>
2017-10-04 15:31:22 +0300

    Initial commit

---
+++ streams.mk
@@ -0,0 +1,9 @@
+# RT Shell files.
+STREAMSSRC = \
+		./chprintf.c \
+        $(CHIBIOS)/os/hal/lib/streams/memstreams.c \
+        $(CHIBIOS)/os/hal/lib/streams/nullstreams.c
+
+STREAMSINC = \
+		./ \
+		$(CHIBIOS)/os/hal/lib/streams
